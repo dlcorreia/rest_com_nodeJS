909d13bd4fd5bceb9ce795561ba687f76fdffe5b
dlcorreia <dlcorreia.ssa@gmail.com>
2021-02-08 23:59:55 +0000

    finished aula02 added a post request and body-parser library

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,5 +1,10 @@
 module.exports = app => {
   app.get('/', (req, res) => res.send('Você está na página inicial'));
   app.get('/atendimentos', (req, res) => res.send('Você está na página de atendimentos'));
+
+  app.post('/atendimentos', (req, res) => {
+    console.log('A requisição enviou: ', req.body);
+    res.send('Você está na rota de atendimentos e realizando um POST')
+  });
 }
 
